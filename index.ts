aacc179a3f13b6077fba7c9dafd362a00d5b4982
Staś Małolepszy <stas@piesku.com>
2019-07-06 15:32:23 +0200

    Biquad filters for each oscillator

--- index.ts
+++ index.ts
@@ -15,7 +15,27 @@ function create_instrument(): Instrument {
         let osc = audio.createOscillator();
         let amp = audio.createGain();
         osc.connect(amp);
-        amp.connect(audio.destination);
+
+        let $filter = document.querySelector(`#osc${i + 1}-filter-enabled`)! as HTMLInputElement;
+        if ($filter.checked) {
+            let $type = document.querySelector(`#osc${i + 1}-filter-type`)! as HTMLInputElement;
+            let $freq = document.querySelector(`#osc${i + 1}-filter-freq`)! as HTMLInputElement;
+            let $q = document.querySelector(`#osc${i + 1}-filter-q`)! as HTMLInputElement;
+
+            let freq = parseFloat($freq.value);
+            let q = parseFloat($q.value);
+
+            let flt = audio.createBiquadFilter();
+            flt.type = $type.value as BiquadFilterType;
+            flt.frequency.value = freq;
+            flt.Q.value = q;
+
+            amp.connect(flt);
+            flt.connect(audio.destination);
+        } else {
+            amp.connect(audio.destination);
+        }
+
         waves.push({osc, amp});
     }
     return {waves};
@@ -25,15 +45,15 @@ function play_instr(instr: Instrument, freq: number, offset: number) {
     let time = audio.currentTime + offset;
 
     for (let [i, wave] of instr.waves.entries()) {
-        let GM = (document.querySelector(`#osc${i + 1}-gain-master`)! as HTMLInputElement).value;
-        let GA = (document.querySelector(`#osc${i + 1}-gain-attack`)! as HTMLInputElement).value;
-        let GS = (document.querySelector(`#osc${i + 1}-gain-sustain`)! as HTMLInputElement).value;
-        let GR = (document.querySelector(`#osc${i + 1}-gain-release`)! as HTMLInputElement).value;
+        let $gm = document.querySelector(`#osc${i + 1}-gain-master`)! as HTMLInputElement;
+        let $ga = document.querySelector(`#osc${i + 1}-gain-attack`)! as HTMLInputElement;
+        let $gs = document.querySelector(`#osc${i + 1}-gain-sustain`)! as HTMLInputElement;
+        let $gr = document.querySelector(`#osc${i + 1}-gain-release`)! as HTMLInputElement;
 
-        let gm = (parseInt(GM) / 9) ** 3;
-        let ga = (parseInt(GA) / 9) ** 3;
-        let gs = (parseInt(GS) / 9) ** 3;
-        let gr = (parseInt(GR) / 6) ** 3;
+        let gm = (parseInt($gm.value) / 9) ** 3;
+        let ga = (parseInt($ga.value) / 9) ** 3;
+        let gs = (parseInt($gs.value) / 9) ** 3;
+        let gr = (parseInt($gr.value) / 6) ** 3;
 
         // wave.amp.gain.cancelScheduledValues(time);
         wave.amp.gain.setValueAtTime(0, time);
@@ -41,21 +61,21 @@ function play_instr(instr: Instrument, freq: number, offset: number) {
         wave.amp.gain.setValueAtTime(gm, time + ga + gs);
         wave.amp.gain.exponentialRampToValueAtTime(0.00001, time + ga + gs + gr);
 
-        let FD = (document.querySelector(`#osc${i + 1}-freq-detune`)! as HTMLInputElement).value;
-        let FA = (document.querySelector(`#osc${i + 1}-freq-attack`)! as HTMLInputElement).value;
-        let FS = (document.querySelector(`#osc${i + 1}-freq-sustain`)! as HTMLInputElement).value;
-        let FR = (document.querySelector(`#osc${i + 1}-freq-release`)! as HTMLInputElement).value;
+        let $fd = document.querySelector(`#osc${i + 1}-freq-detune`)! as HTMLInputElement;
+        let $fa = document.querySelector(`#osc${i + 1}-freq-attack`)! as HTMLInputElement;
+        let $fs = document.querySelector(`#osc${i + 1}-freq-sustain`)! as HTMLInputElement;
+        let $fr = document.querySelector(`#osc${i + 1}-freq-release`)! as HTMLInputElement;
 
-        let fd = parseInt(FD) ** 3;
-        let fa = (parseInt(FA) / 9) ** 3;
-        let fs = (parseInt(FS) / 9) ** 3;
-        let fr = (parseInt(FR) / 6) ** 3;
+        let fd = parseInt($fd.value) ** 3;
+        let fa = (parseInt($fa.value) / 9) ** 3;
+        let fs = (parseInt($fs.value) / 9) ** 3;
+        let fr = (parseInt($fr.value) / 6) ** 3;
 
         // wave.osc.frequency.cancelScheduledValues(time);
         wave.osc.detune.setValueAtTime(fd, time);
 
-        let freq_env = document.querySelector(`#osc${i + 1}-freq-env`)! as HTMLInputElement;
-        if (freq_env.checked) {
+        let $freq_env = document.querySelector(`#osc${i + 1}-freq-env`)! as HTMLInputElement;
+        if ($freq_env.checked) {
             wave.osc.frequency.linearRampToValueAtTime(0, time);
             wave.osc.frequency.linearRampToValueAtTime(freq, time + fa);
             wave.osc.frequency.setValueAtTime(freq, time + fa + fs);
