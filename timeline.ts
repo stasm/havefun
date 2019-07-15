cddf2dd7896d907f3a6516c6e96011342669fc48
Staś Małolepszy <stas@piesku.com>
2019-07-15 21:27:55 +0200

    Draw a track

--- timeline.ts
+++ timeline.ts
@@ -12,19 +12,20 @@ game.add({
     using: [zoom(1.1), pan(1)],
     children: [
         {
-            translation: [100, 100],
-            scale: [1, 2],
-            using: [render_rect(10, 10, "#f00"), selectable()],
-        },
-        {
-            translation: [110, 120],
-            scale: [1, 2],
-            using: [render_rect(10, 10, "#f00"), selectable()],
-        },
-        {
-            translation: [130, 140],
-            scale: [1, 2],
-            using: [render_rect(10, 10, "#f00"), selectable()],
+            translation: [0, 100],
+            using: [render_rect(480, 100, "#fff")],
+            children: [
+                {
+                    translation: [100, 0],
+                    scale: [1, 2],
+                    using: [render_rect(10, 10, "#f00"), selectable()],
+                },
+                {
+                    translation: [110, 20],
+                    scale: [1, 2],
+                    using: [render_rect(10, 10, "#f00"), selectable()],
+                },
+            ],
         },
     ],
 });
