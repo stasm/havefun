aa53c0899fc621945e6bc1a80a73f15a243ca0bb
Staś Małolepszy <stas@piesku.com>
2019-07-15 10:42:56 +0200

    Cache AABBs

--- timeline/com_selectable.ts
+++ timeline/com_selectable.ts
@@ -3,11 +3,17 @@ import {Entity, Game} from "./game.js";
 
 export interface Selectable extends BaseComponent {
     selected: boolean;
+    aabb?: AABB;
 }
 
 export function selectable() {
     return (game: Game) => (entity: Entity) => {
         game.world[entity] |= Get.Selectable;
-        game[Get.Selectable][entity] = <Selectable>{entity};
+        game[Get.Selectable][entity] = <Selectable>{
+            entity,
+            selected: false,
+        };
     };
 }
+
+export type AABB = [number, number, number, number];
--- timeline/sys_intersect.ts
+++ timeline/sys_intersect.ts
@@ -1,5 +1,6 @@
 import {Get} from "./com_index.js";
 import {RenderRect} from "./com_render.js";
+import {AABB} from "./com_selectable.js";
 import {Game} from "./game.js";
 import {Mat2D, Vec2} from "./mth_index.js";
 import {transform_point} from "./mth_mat2d.js";
@@ -22,14 +23,18 @@ export function sys_intersect(game: Game) {
         for (let i = 0; i < game.world.length; i++) {
             if ((game.world[i] & QUERY) === QUERY) {
                 let selectable = game[Get.Selectable][i];
-                let selectable_transform = game[Get.Transform][i];
-                let selectable_render = game[Get.Render][i] as RenderRect;
-                let selectable_aabb = compute_aabb(
-                    [0, 0],
-                    [selectable_render.width, selectable_render.height],
-                    selectable_transform.world
-                );
-                if (intersect_aabb(selection_aabb, selectable_aabb)) {
+
+                if (!selectable.aabb) {
+                    let selectable_transform = game[Get.Transform][i];
+                    let selectable_render = game[Get.Render][i] as RenderRect;
+                    selectable.aabb = compute_aabb(
+                        [0, 0],
+                        [selectable_render.width, selectable_render.height],
+                        selectable_transform.world
+                    );
+                }
+
+                if (intersect_aabb(selection_aabb, selectable.aabb)) {
                     selectable.selected = true;
                 } else {
                     selectable.selected = false;
@@ -39,14 +44,11 @@ export function sys_intersect(game: Game) {
     }
 }
 
-type AABB = [number, number, number, number];
-
 function compute_aabb(top_left: Vec2, bottom_right: Vec2, matrix: Mat2D) {
-    let aabb = [
+    return [
         ...transform_point(top_left, top_left, matrix),
         ...transform_point(bottom_right, bottom_right, matrix),
     ] as AABB;
-    return aabb;
 }
 
 function intersect_aabb(a: AABB, b: AABB) {
