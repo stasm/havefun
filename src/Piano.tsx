67324fa211893b19efa31aabbbfb60b470b1a92c
Staś Małolepszy <stas@piesku.com>
2019-11-12 11:02:40 +0100

    Ref instr so that MIDI input always uses the current one

--- src/Piano.tsx
+++ src/Piano.tsx
@@ -4,13 +4,20 @@ import {Instrument, play_instr} from "./player";
 
 export function Piano({instr}: {instr: Instrument}) {
     let [audio] = React.useState(new AudioContext());
+    let instr_ref = React.useRef(instr);
+    instr_ref.current = instr;
+
     React.useEffect(() => {
         if (navigator.requestMIDIAccess) {
-            request_midi(audio, instr);
+            request_midi(play_note);
         }
     }, []);
 
-    function play(evt: React.MouseEvent) {
+    function play_note(note: number) {
+        play_instr(audio, instr_ref.current, note, 0);
+    }
+
+    function play_key(evt: React.MouseEvent) {
         let note = evt.currentTarget.getAttribute("data-note")!;
         play_instr(audio, instr, parseInt(note), 0);
     }
@@ -19,43 +26,43 @@ export function Piano({instr}: {instr: Instrument}) {
         <div className="group">
             <h2>Piano</h2>
             <div className="row">
-                <button className="key white" onClick={play} data-note="48"></button>
-                <button className="key black" onClick={play} data-note="49"></button>
-                <button className="key white" onClick={play} data-note="50"></button>
-                <button className="key black" onClick={play} data-note="51"></button>
-                <button className="key white" onClick={play} data-note="52"></button>
-                <button className="key white" onClick={play} data-note="53"></button>
-                <button className="key black" onClick={play} data-note="54"></button>
-                <button className="key white" onClick={play} data-note="55"></button>
-                <button className="key black" onClick={play} data-note="56"></button>
-                <button className="key white" onClick={play} data-note="57"></button>
-                <button className="key black" onClick={play} data-note="58"></button>
-                <button className="key white" onClick={play} data-note="59"></button>
-                <button className="key white" onClick={play} data-note="60"></button>
-                <button className="key black" onClick={play} data-note="61"></button>
-                <button className="key white" onClick={play} data-note="62"></button>
-                <button className="key black" onClick={play} data-note="63"></button>
-                <button className="key white" onClick={play} data-note="64"></button>
-                <button className="key white" onClick={play} data-note="65"></button>
-                <button className="key black" onClick={play} data-note="66"></button>
-                <button className="key white" onClick={play} data-note="67"></button>
-                <button className="key black" onClick={play} data-note="68"></button>
-                <button className="key white" onClick={play} data-note="69"></button>
-                <button className="key black" onClick={play} data-note="70"></button>
-                <button className="key white" onClick={play} data-note="71"></button>
-                <button className="key white" onClick={play} data-note="72"></button>
-                <button className="key black" onClick={play} data-note="73"></button>
-                <button className="key white" onClick={play} data-note="74"></button>
-                <button className="key black" onClick={play} data-note="75"></button>
-                <button className="key white" onClick={play} data-note="76"></button>
-                <button className="key white" onClick={play} data-note="77"></button>
-                <button className="key black" onClick={play} data-note="78"></button>
-                <button className="key white" onClick={play} data-note="79"></button>
-                <button className="key black" onClick={play} data-note="80"></button>
-                <button className="key white" onClick={play} data-note="81"></button>
-                <button className="key black" onClick={play} data-note="82"></button>
-                <button className="key white" onClick={play} data-note="83"></button>
-                <button className="key white" onClick={play} data-note="84"></button>
+                <button className="key white" onClick={play_key} data-note="48"></button>
+                <button className="key black" onClick={play_key} data-note="49"></button>
+                <button className="key white" onClick={play_key} data-note="50"></button>
+                <button className="key black" onClick={play_key} data-note="51"></button>
+                <button className="key white" onClick={play_key} data-note="52"></button>
+                <button className="key white" onClick={play_key} data-note="53"></button>
+                <button className="key black" onClick={play_key} data-note="54"></button>
+                <button className="key white" onClick={play_key} data-note="55"></button>
+                <button className="key black" onClick={play_key} data-note="56"></button>
+                <button className="key white" onClick={play_key} data-note="57"></button>
+                <button className="key black" onClick={play_key} data-note="58"></button>
+                <button className="key white" onClick={play_key} data-note="59"></button>
+                <button className="key white" onClick={play_key} data-note="60"></button>
+                <button className="key black" onClick={play_key} data-note="61"></button>
+                <button className="key white" onClick={play_key} data-note="62"></button>
+                <button className="key black" onClick={play_key} data-note="63"></button>
+                <button className="key white" onClick={play_key} data-note="64"></button>
+                <button className="key white" onClick={play_key} data-note="65"></button>
+                <button className="key black" onClick={play_key} data-note="66"></button>
+                <button className="key white" onClick={play_key} data-note="67"></button>
+                <button className="key black" onClick={play_key} data-note="68"></button>
+                <button className="key white" onClick={play_key} data-note="69"></button>
+                <button className="key black" onClick={play_key} data-note="70"></button>
+                <button className="key white" onClick={play_key} data-note="71"></button>
+                <button className="key white" onClick={play_key} data-note="72"></button>
+                <button className="key black" onClick={play_key} data-note="73"></button>
+                <button className="key white" onClick={play_key} data-note="74"></button>
+                <button className="key black" onClick={play_key} data-note="75"></button>
+                <button className="key white" onClick={play_key} data-note="76"></button>
+                <button className="key white" onClick={play_key} data-note="77"></button>
+                <button className="key black" onClick={play_key} data-note="78"></button>
+                <button className="key white" onClick={play_key} data-note="79"></button>
+                <button className="key black" onClick={play_key} data-note="80"></button>
+                <button className="key white" onClick={play_key} data-note="81"></button>
+                <button className="key black" onClick={play_key} data-note="82"></button>
+                <button className="key white" onClick={play_key} data-note="83"></button>
+                <button className="key white" onClick={play_key} data-note="84"></button>
             </div>
         </div>
     );
--- src/midi.ts
+++ src/midi.ts
@@ -1,22 +1,15 @@
-import {Instrument, play_instr} from "./player";
-
-export function request_midi(audio: AudioContext, instr: Instrument) {
+export function request_midi(play_note: (note: number) => void) {
     navigator
         .requestMIDIAccess()
         .then(midi => {
             for (let input of midi.inputs.values()) {
-                console.log(input);
-                input.onmidimessage = message => on_midi_message(audio, instr, message);
+                input.onmidimessage = message => on_midi_message(play_note, message);
             }
         })
         .catch(console.error);
 }
 
-function on_midi_message(
-    audio: AudioContext,
-    instr: Instrument,
-    message: WebMidi.MIDIMessageEvent
-) {
+function on_midi_message(play_note: (note: number) => void, message: WebMidi.MIDIMessageEvent) {
     let [command, note, velocity] = message.data;
     switch (command & 0xf0) {
         case 240:
@@ -24,7 +17,7 @@ function on_midi_message(
         case 144: {
             let button = document.querySelector(`button.key[data-note="${note}"]`);
             if (velocity > 0) {
-                play_instr(audio, instr, note, 0);
+                play_note(note);
                 button && button.classList.add("pressed");
             } else {
                 button && button.classList.remove("pressed");
